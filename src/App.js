8978cd8f6e47375b908456ec692f562a2087296b
Yevhenii Maksimishin <evgenijmaksimisin@gmail.com>
2021-11-25 00:31:22 +0200

    setting up routing

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import './App.css';
+import { Routes, Route } from "react-router-dom";
+import HomePage from './pages/HomePage/HomePage.js';
+import LoginPage from './pages/LoginPage/LoginPage.js';
+import RegisterPage from './pages/RegisterPage/RegisterPage.js';
+
+
+
 
 function App() {
   return (
-    <div className="App">
-      <h1>Test</h1>
-    </div>
+    <>
+      <Routes>
+        <Route exact path="/" element={<HomePage />} />
+        <Route exact path="/login" element={<LoginPage />} />
+        <Route exact path="/register" element={<RegisterPage />} />
+      </Routes>
+    </>
   );
 }
 
