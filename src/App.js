7ebe14e7a5be4d0b5557747c192a195c6a95730d
Yevhenii Maksimishin <evgenijmaksimisin@gmail.com>
2021-11-25 01:22:23 +0200

    setting up redux

--- src/App.js
+++ src/App.js
@@ -9,14 +9,14 @@ import RegisterPage from './pages/RegisterPage/RegisterPage.js';
 
 function App() {
   return (
-    <>
+    <div className="app">
       <Routes>
         <Route exact path="/" element={<HomePage />} />
         <Route exact path="/login" element={<LoginPage />} />
         <Route exact path="/register" element={<RegisterPage />} />
         <Route path="*" element={<HomePage/>}/>
       </Routes>
-    </>
+    </div>
   );
 }
 
